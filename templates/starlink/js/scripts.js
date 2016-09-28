c559666a09b1361f77f2470de28d4714510011ab
mao <green.turban@gmail.com>
2016-09-28 12:05:34 +0300

    database; solutions pages and services pages fully refactored to BEM; sticky menu in services - disabled

--- templates/starlink/js/scripts.js
+++ templates/starlink/js/scripts.js
@@ -145,25 +145,25 @@ jQuery(document).ready(function($) {
             'slow');
     });
 
-    // TODO remove comments after checking
-    jQuery(window).scroll(function(){
-        var headerAndTopBlHeight = jQuery("header").innerHeight() /*+ jQuery(".servicesBgBl1Margin").innerHeight()*/;
-        if (jQuery(".secondaryConsultingMenuBl").length > 0) {    // TODO move this into 2nd .sticky addition instruction
-            if(jQuery(window).scrollTop() > headerAndTopBlHeight) {
-              /*jQuery('.scrollFixedConsultingBl').css('position', 'fixed');
-              jQuery('body').css('margin-top', '200px');
-              jQuery('.scrollBl').slideDown();*/
-              jQuery('body>div.custom-services-image-header').addClass('sticky');
-              jQuery('.scrollFixedConsultingBl').addClass('sticky');
-            } else {
-              jQuery('body>div.custom-services-image-header').removeClass('sticky');
-              jQuery('.scrollFixedConsultingBl').removeClass('sticky');
-              /*jQuery('.scrollFixedConsultingBl').css('position', 'relative');
-              jQuery('body').css('margin-top', '0');
-              jQuery('.scrollBl').hide();*/
-            }
-        }
-    });
+ // // TODO remove comments after checking
+ // jQuery(window).scroll(function(){
+ //     var headerAndTopBlHeight = jQuery("header").innerHeight() /*+ jQuery(".servicesBgBl1Margin").innerHeight()*/;
+ //     if (jQuery(".secondaryConsultingMenuBl").length > 0) {    // TODO move this into 2nd .sticky addition instruction
+ //         if(jQuery(window).scrollTop() > headerAndTopBlHeight) {
+ //           /*jQuery('.scrollFixedConsultingBl').css('position', 'fixed');
+ //           jQuery('body').css('margin-top', '200px');
+ //           jQuery('.scrollBl').slideDown();*/
+ //           jQuery('body>div.custom-services-image-header').addClass('sticky');
+ //           jQuery('.scrollFixedConsultingBl').addClass('sticky');
+ //         } else {
+ //           jQuery('body>div.custom-services-image-header').removeClass('sticky');
+ //           jQuery('.scrollFixedConsultingBl').removeClass('sticky');
+ //           /*jQuery('.scrollFixedConsultingBl').css('position', 'relative');
+ //           jQuery('body').css('margin-top', '0');
+ ////           jQuery('.scrollBl').hide();*/
+ //         }
+ //     }
+ // });
 
     // Wrap every 3 news to block
     var newsBlock = jQuery('.blog>.row');
