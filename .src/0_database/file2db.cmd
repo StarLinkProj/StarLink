a6786472ea5a287ccd2844a361a6c9e33a44999d
mao <green.turban@gmail.com>
2017-01-20 15:33:28 +0200

    Build&config: database utilities

--- .src/0_database/file2db.cmd
+++ .src/0_database/file2db.cmd
@@ -3,7 +3,7 @@ SET _port=13306
 SET _database=starlink.dev
 SET _user=dev
 
-gunzip < %1 | mysql -h%_host% -P%_port% -u%_user% -pdev --database=%_database%
+gzip -d < %1 | mysql -h%_host% -P%_port% -u%_user% -pdev --database=%_database%
 
 @ECHO OFF
 SET _host=
